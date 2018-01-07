7132edc22dd725a92ec7cde6d42b80238fead828
zengtianshengz <zengtianshengz@163.com>
2018-01-07 15:28:06 +0800

    nt

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,16 +1,22 @@
-// const utils = require('../libs/lib/utils');
+const storage = require('../libs/storage')
+
 const FIX_URL = 'https://yqy.mynatapp.cc'
 const req = function (url, data, method = 'POST') {
     const _url = `${FIX_URL}${url}`
-    // const options = utils.extend({}, data);
     return new Promise((resolve, reject) => {
+        let header = {
+            'Accept': 'application/json',
+            'content-type': 'application/json' // 默认值
+        }
+        const token = storage.get('XCX-Admin') || ''
+        if (token) {
+            header.Authorization = `Bearer ${token}`
+        }
         wx.request({
             url: _url,
             method: method,
             data: data,
-            header: {
-                'content-type': 'application/json' // 默认值
-            },
+            header,
             success: function (res) {
                 resolve(res.data)
             },
